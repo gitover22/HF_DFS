7fece2dba29c89c01d60198963a39c612d13f705
gitover22 <qidizou88@gmail.com>
2024-06-07 13:47:26 +0800

    update

--- main.c
+++ main.c
@@ -7,9 +7,9 @@
 #include "fdfs_upload.h"
 int main(int argc, char *argv[]){
     char fileID[1024]={0};
-    upload_file1("/etc/fdfs/client.conf",argv[1],fileID);
+    upload_file1("/etc/fdfs/client.conf.sample",argv[1],fileID);
     printf("%s\n",fileID);
-    upload_file2("/etc/fdfs/client.conf",argv[1],fileID,sizeof(fileID));
+    upload_file2("/etc/fdfs/client.conf.sample",argv[1],fileID,sizeof(fileID));
     printf("%s\n",fileID);
     return 0;
 }
