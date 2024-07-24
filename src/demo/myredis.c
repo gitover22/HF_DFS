a812e302d63a262a21c9375853b9a743cd424974
gitover22 <qidizou88@gmail.com>
2024-07-24 21:28:40 +0800

    modify demos

--- src/demo/myredis.c
+++ src/demo/myredis.c
@@ -1,5 +1,8 @@
+/**
+ * @brief redis客户端程序示例
+ */
 #include <stdio.h>
-#include <hiredis.h>
+#include <hiredis.h> // 引入hiredis头文件
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -12,7 +15,7 @@ int main(int argc, const char* argv[])
         exit(1);
     }
 
-    // 连接redis服务器
+    // 连接
     redisContext * res = redisConnect("127.0.0.1", atoi(argv[1]));
     if(res == NULL)
     {
@@ -20,20 +23,19 @@ int main(int argc, const char* argv[])
         exit(1);
     }
 
-    // 测试
+    // 测试ping
     redisReply* ret = NULL;
     ret = redisCommand(res, "ping");
     freeReplyObject(ret);
 
-    // 存储一个hash
+    // 执行
     ret = redisCommand(res, "hset user name %s", "kevin");
     freeReplyObject(ret);
     ret = redisCommand(res, "hset user age %d", 34);
     freeReplyObject(ret);
     ret = redisCommand(res, "hset user passwd %s", "123abc");
     freeReplyObject(ret);
-
-    // 释放资源
+    // 释放
     redisFree(res);
 
     return 0;
